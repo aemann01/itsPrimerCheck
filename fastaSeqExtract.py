8a6cbda0d8e74f70f0ab581e14eda8f0fa87e8d0
aemann <allison.e.mann@gmail.com>
2017-11-16 20:44:18 -0600

    Pulls ITS flanking RNA seqs from RNA ref fasta, for ITS database creation

---
+++ fastaSeqExtract.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+'''For filtering out RNA seqs out of fasta files. Useage: python fastaSeqExtract.py in.fasta out.fasta'''
+
+import sys
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+
+rnafa = []
+
+for record in SeqIO.parse(sys.argv[1], "fasta"):
+	if "S ribosomal RNA " and not "mRNA" in record.description:
+		rnafa.append(record)
+
+with open(sys.argv[2], "w") as handle:
+	SeqIO.write(rnafa, handle, "fasta")
