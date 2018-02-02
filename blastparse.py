4112e382dd36325c011702b9c292b3d4a92fe9d1
aemann <allison.e.mann@gmail.com>
2018-02-02 16:23:08 -0600

    fixed float issue

--- blastparse.py
+++ blastparse.py
@@ -4,5 +4,4 @@ from Bio.Blast import NCBIXML
 for rec in NCBIXML.parse(open("its2.blast.out.xml")):
 	for alignment in rec.alignments:
 		for hsp in alignment.hsps:
-			print("%s\t%s\t%s\t%f" % (alignment.title, alignment.length, hsp.identities, alignment.length))
-
+			print("%s\t%s\t%s\t%i\f%i\t%f" % (rec.query, alignment.title, alignment.length, hsp.identities, alignment.length, (hsp.identities/alignment.length)))
